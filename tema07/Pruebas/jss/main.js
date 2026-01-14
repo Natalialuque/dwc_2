6d44bc8a764fa14d017a84180e4b29279ff0f19b
Natalialuque <ncabluq@g.educaand.es>
2026-01-14 09:22:40 +0100

    Primer commit

---
+++ tema07/Pruebas/jss/main.js
@@ -0,0 +1,7 @@
+let info = document.getElementById("info");
+
+info.innerHTML="Hola mundo";
+
+document.getElementById("leer").addEventListener("click",()=>{
+    let xhr = new XMLHttpRequest();
+});
