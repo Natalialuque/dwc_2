b4e67005c936f64b272596f81cee763ebca33c60
Natalialuque <ncabluq@g.educaand.es>
2026-01-22 10:30:47 +0100

    22/01/2026

--- tema07/Pruebas/jss/main.js
+++ tema07/Pruebas/jss/main.js
@@ -20,35 +20,73 @@
 
 
 /////////////////////////////////////////////////////////////
-let info = document.getElementById("info");
+ let info = document.getElementById("info");
 
 
-//al enlace le damos evento para que cuando lo pulsemos redirija
-document.getElementById("leer").addEventListener("click",()=>{
-    let xhr = new XMLHttpRequest();
+// //al enlace le damos evento para que cuando lo pulsemos redirija
+// document.getElementById("leer").addEventListener("click",()=>{
+//     let xhr = new XMLHttpRequest();
 
-    //xhr.responseType="text";
-    xhr.onreadystatechange = function(){
-        if((xhr.readyState == 4) && (xhr.status==200)){
-            //procesamos respuesta del server
-            var alumnos = xhr.responseXML;
-            alumnos = alumnos.getElementsByTagName("usuarios")[0].getElementsByTagName("usuario");
-            console.dir("nombre: "+alumnos[1].getElementsByTagName("nombre")[0].textContent);
-            info.innerHTML="Nombre :"+alumnos[1].getElementsByTagName("nombre")[0].textContent;
-        }
-    }
+//     //xhr.responseType="text";
+//     xhr.onreadystatechange = function(){
+//         if((xhr.readyState == 4) && (xhr.status==200)){
+//             //procesamos respuesta del server
+//             var alumnos = xhr.responseXML;
+//             alumnos = alumnos.getElementsByTagName("usuarios")[0].getElementsByTagName("usuario");
+//             console.dir("nombre: "+alumnos[1].getElementsByTagName("nombre")[0].textContent);
+//             info.innerHTML="Nombre :"+alumnos[1].getElementsByTagName("nombre")[0].textContent;
+//         }
+//     }
 
-    //esto establece la conexion con lo que queremos mostrar
-    //xhr.open("GET","http://localhost/DWC/tema07/Pruebas/server/hello.php?login=pepe&pass=1234");
-    xhr.open("GET","http://localhost/DWC/tema07/Pruebas/server/alumnos.xml");
+//     //esto establece la conexion con lo que queremos mostrar
+//     //xhr.open("GET","http://localhost/DWC/tema07/Pruebas/server/hello.php?login=pepe&pass=1234");
+//     xhr.open("GET","http://localhost/DWC/tema07/Pruebas/server/alumnos.xml");
 
-    //ESTABLECER CABECERA AL ENVIAR DATOS ALWAYS
-   // xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    //xhr.send("login=pepe&pass=1234");
-    xhr.send();
+//     //ESTABLECER CABECERA AL ENVIAR DATOS ALWAYS
+//    // xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+//     //xhr.send("login=pepe&pass=1234");
+//     xhr.send();
+//     //xhr.send(JSON.stringify(alumnos)); PARA PODER ENVIAR EL JSON COMO CADENA
 
 
-});
+// });
 
 
 //vamos a hacer lo mismo con jSon
+
+
+/////
+/**
+ * FECTH
+ */
+document.getElementById("leer").addEventListener("click",()=>{
+
+    const resp=fetch("http://localhost/DWC/tema07/Pruebas/server/hello.php",{
+        //para hacer get quitamos el method, dejamos headers y el body entra en el url de arriba con un ?
+
+        method:"POST",
+        headers:{'Content-Type': 'application/x-www-form-urlencoded'},
+        body:"usuario=Pepe&pass=1234"
+    });
+
+    resp.then(function(resp){
+        if(resp.ok){
+            info.innerHTML="Respuesta"+resp;
+            console.log(resp);
+
+            resp.text()
+            .then(function(data){
+                info.innerHTML=" valor:"+data;
+            }).catch(function(err){
+                info.innerHTML="ERROR"+err;
+
+            });
+        }
+        
+    })
+    .catch(function(err){
+        info.innerHTML="ERROR"+err;
+    });
+
+
+});
--- tema07/Pruebas/server/hello.php
+++ tema07/Pruebas/server/hello.php
@@ -1,8 +1,24 @@
 <?php
 
- $login = $_GET["login"];
- $pass = $_GET["pass"];
 
- echo "Recibido login:" .$login." en el servidor.El password<br>".$pass.""
+$nombre =$_POST["usuario"];
+$pass =$_POST["pass"];
+
+echo "recibdo nombre" .$nombre."en el server";
+
+// $data=file_get_contents('php://inputs');
+
+// $data = json_decode($data,false);
+
+// $data2['nombre']="peito";
+// $data2['rol']="Administrador";
+
+// echo json_encode($data2);
+
+//  $login = $_GET["login"];
+//  $pass = $_GET["pass"];
+
+
+//  echo "Recibido login:" .$login." en el servidor.El password<br>".$pass.""
 
 ?>
