93e39b407fca3e33e4faae186eb5afdc4d911e7e
Natalialuque <ncabluq@g.educaand.es>
2026-01-28 12:00:35 +0100

    dwc 28/01/2026

--- tema08/pruebas/js/main.js
+++ tema08/pruebas/js/main.js
@@ -6,32 +6,104 @@ let login = document.getElementById("login");
 let pass = document.getElementById("pass");
 let color = document.getElementById("color");
 
-boton.onclick=function(){
 
-console.log("Login: "+login.value+" Pass: "+pass.value);
 
-//los campos de una cockie van con ; para separarlo
-document.cookie="Login="+login.value 
-document.cookie="Pass="+pass.value;
 
-//creamos una cockie de color 
-document.cookie = "Color="+color.value;
+if (checkCookie("login")) 
+    login.value = checkCookie("login");
+if (checkCookie("pass")) 
+    pass.value = checkCookie("pass");
+if (checkCookie("color"))
+    document.body.style.backgroundColor = checkCookie("color"); 
 
+
+// Pulsamos botÃ³n de Login
+document.getElementById("boton").addEventListener("click", () => {
+    const login = document.getElementById("login");
+    const pass = document.getElementById("pass");
+    const color = document.getElementById("color");
+
+    console.log("Login: " + login.value + ", Password: " + pass.value);
+
+    document.cookie = "login=" + login.value + ";max-age=" + (24*60*60);
+    document.cookie = "pass=" + pass.value;
+    document.cookie = "color=" + color.value;
+
+    let colores = checkCookie("color");
+    if (colores)
+        document.body.style.backgroundColor = colores;    
+});
+
+
+function checkCookie(c) {
+    let result = false;
+    if (document.cookie.match(c)) {
+        let myCookies = document.cookie.split(";");
+        for (let i = 0; i < myCookies.length; i++)
+            if (myCookies[i].match(c)) {
+                let color = myCookies[i].split("=");
+                result = color[1];
+                break;
+            } 
+    }
+    else {
+        result = false;
+    }
+
+    return result;
+}
+///////////////////////////////////////////////////////////////////////////////
+// Pulsamos botÃ³n de Login
+
+// Pulsamos botÃ³n de Login
+document.getElementById("boton").addEventListener("click", () => {
+    const login = document.getElementById("login");
+    const pass = document.getElementById("pass");
+    const prefColor = document.getElementById("color");
+
+    sessionStorage.setItem("login", login.value);
+    sessionStorage.setItem("pass", pass.value);
+    sessionStorage.setItem("prefColor", prefColor.value);
+});
+
+document.getElementById("bErase").onclick = function() {
+    sessionStorage.clear();
+
+    document.getElementById("login").value = "";
+    document.getElementById("pass").value = "";
+    document.getElementById("color").value = "";
 
 }
 
-function getCookie(nombre) {
-    const partes = document.cookie.split("; ");
-    for (let parte of partes) {
-        const [key, value] = parte.split("=");
-        if (key === nombre) return value;
-    }
-    return null;
+/************************************ */
+let info = document.getElementById("info");
+const login = document.getElementById("login");
+const pass = document.getElementById("pass");
+const prefColor = document.getElementById("prefColor");
+
+if (localStorage.length) {
+
+    if (localStorage.login)
+        login.value = localStorage.login;
+    else
+        login.placeholder = "Introduzca login";
+
+    if (localStorage.pass)
+        pass.value = localStorage.pass;
+
+    if (localStorage.prefColor)
+        document.body.style.backgroundColor =  localStorage.prefColor;
+
+}
+else{
+    console.info("No existen variables en localStorage");
 }
 
-window.onload = function() {
-    const color = getCookie("Color");
-    if (color) {
-        document.body.style.backgroundColor = color;
-    }
-};
+/************************************ */
+// Pulsamos botÃ³n de Login
+document.getElementById("boton").addEventListener("click", () => {
+    localStorage.setItem("login",login.value);
+    localStorage.pass = pass.value;
+    localStorage.prefColor = prefColor.value;
+});
+
