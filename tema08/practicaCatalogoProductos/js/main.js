afc1da9c43851320804838a19899659c2b96621c
Natalialuque <ncabluq@g.educaand.es>
2026-02-03 14:13:33 +0100

    diw 03/02/2026

---
+++ tema08/practicaCatalogoProductos/js/main.js
@@ -0,0 +1,19 @@
+/**MOSTRAR  */
+
+const { createElement } = require("react");
+
+    //variable para variar tarjetas
+    let tarjetas = document.getElementById("tarjetas");
+
+    //funcion para cargar los productos
+
+
+    //tarjetas con create elementos 
+    function crearTarjetaProducto(producto) {
+        const tarjetas = createElement("div");
+
+  
+    }
+
+
+
